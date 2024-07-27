516d8a8b523c534fcc456c1760227d7bc2585d71
Leevi Alanen <leevi.alanen1@gmail.com>
2024-07-27 14:49:44 +0300

    exercise 3.4 complete

--- index.js
+++ index.js
@@ -49,7 +49,14 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
-  
+
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
 })
 
 const PORT = 3001
